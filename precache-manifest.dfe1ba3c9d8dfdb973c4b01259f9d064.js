9f1b0d9fa746d8f72a41d8315741faf5c4e7e2f4
Giorgio Armani <george.v.kalashnikov@gmail.com>
2022-08-31 18:02:52 -0400

    Updates

---
+++ precache-manifest.dfe1ba3c9d8dfdb973c4b01259f9d064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "085302e5855e5a046360",
+    "url": "/app-form-assignment/static/css/main.65b7e5bd.chunk.css"
+  },
+  {
+    "revision": "085302e5855e5a046360",
+    "url": "/app-form-assignment/static/js/main.085302e5.chunk.js"
+  },
+  {
+    "revision": "e01822950b602a906fe9",
+    "url": "/app-form-assignment/static/js/1.e0182295.chunk.js"
+  },
+  {
+    "revision": "3857dc1774ee4cb0e203",
+    "url": "/app-form-assignment/static/js/2.3857dc17.chunk.js"
+  },
+  {
+    "revision": "82b4f5feda54a44b5f17",
+    "url": "/app-form-assignment/static/js/runtime~main.82b4f5fe.js"
+  },
+  {
+    "revision": "0351fd25b57572119fa0f6395a8d665d",
+    "url": "/app-form-assignment/index.html"
+  }
+];
